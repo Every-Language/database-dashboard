b731f0fc40c22ac7943f957c1113365f7bfccf9a
ryderwishart <ryderwishart@gmail.com>
2025-03-27 10:27:36 -0600

    chore: update profile dropdown and sidebar data for public user representation
    - Updated user name and email in sidebar data to 'Public' and 'public@gmail.com'.
    - Changed avatar fallback text in profile dropdown from 'SN' to 'EL'.
    - Modified dropdown content to reflect public user status.
    - Added .env to .gitignore to prevent environment variable files from being tracked.
    - Commented out unused components and sections in the dashboard for cleaner code.

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -19,17 +19,15 @@ export function ProfileDropdown() {
         <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
           <Avatar className='h-8 w-8'>
             <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-            <AvatarFallback>SN</AvatarFallback>
+            <AvatarFallback>EL</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>satnaing</p>
-            <p className='text-xs leading-none text-muted-foreground'>
-              satnaingdev@gmail.com
-            </p>
+            <p className='text-sm font-medium leading-none'>Public</p>
+            <p className='text-xs leading-none text-muted-foreground'>public</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
@@ -40,25 +38,25 @@ export function ProfileDropdown() {
               <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem asChild>
+          {/* <DropdownMenuItem asChild>
             <Link to='/settings'>
               Billing
               <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
             </Link>
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
           <DropdownMenuItem asChild>
             <Link to='/settings'>
               Settings
               <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
+          {/* <DropdownMenuItem>New Team</DropdownMenuItem> */}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        {/* <DropdownMenuItem>
           Log out
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
       </DropdownMenuContent>
     </DropdownMenu>
   )
